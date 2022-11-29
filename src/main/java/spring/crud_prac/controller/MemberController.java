4412041cd052bcf7b5a460321b8b041d50ae4c6d
임아영 <https://github.com/a0lim/SPRING.git>
2022-11-29 19:53:13 +0900

    feat: create members.html and mapping with controller

--- src/main/java/spring/crud_prac/controller/MemberController.java
+++ src/main/java/spring/crud_prac/controller/MemberController.java
@@ -2,6 +2,7 @@ package spring.crud_prac.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import spring.crud_prac.service.MemberService;
 
 @Controller
@@ -14,4 +15,10 @@ public class MemberController {
     public MemberController(MemberService memberService){
         this.memberService = memberService;
     }
+
+    @GetMapping("/create")
+    public String createMember(){
+        return "/create/createMember";
+    }
+
 }
